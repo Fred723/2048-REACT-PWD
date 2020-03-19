c4eb56c8cbf9abc38537f6f4cd4ddfa123ae094c
Sakhiri Mostafa <mostafa.sakhiri@gmail.com>
2020-03-19 15:53:11 +0100

    Use cellsBoard component in dashboard

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from 'react'
+import CellsBoard from '../components/cellsBoard/index'
+
+const board = [
+  [4, 16, 512, 256, 32, 8],
+  [null, 32, null, null, 4, 2],
+  [2, null, 2, 32, 2, 4],
+  [8, 8, 4, 256, null, 8],
+  [16, 16, 8, null, null, 256],
+  [32, null, 256, 32, 2, 4]
+]
 
 const dashboard = () => {
-    return (
-        <div>
-            dashboard
-        </div>
-    );
+  return (
+    <div>
+      <CellsBoard board={board} />
+    </div>
+  );
 };
 
 export default dashboard;
