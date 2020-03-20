14c06064e0db9d52ab1cebeb2fa76d9d35e19278
Sakhiri Mostafa <mostafa.sakhiri@gmail.com>
2020-03-20 11:41:24 +0100

    Add redux reducers

---
+++ src/reducers/board.js
@@ -0,0 +1,25 @@
+import { REFRESH_BOARD } from '../actions/board'
+
+const initialState = {
+  board: [
+    [4, 16, 512, 256, 32, 8],
+    [null, 32, null, null, 4, 2],
+    [2, null, 2, 32, 2, 4],
+    [8, 8, 4, 256, null, 8],
+    [16, 16, 8, null, null, 256],
+    [32, null, 256, 32, 2, 4]
+  ]
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case REFRESH_BOARD:
+      return {
+        ...state,
+        board: state.board
+      }
+
+    default:
+      return state
+  }
+}
