46afc31363cc2e18cea7296766742361734e071f
Sakhiri Mostafa <mostafa.sakhiri@gmail.com>
2020-03-20 18:17:35 +0100

    Move themeprovider to routes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from 'react'
 import Routes from './config/routes'
 import { Provider } from 'react-redux'
 import { store } from './config/store'
-import { ThemeProvider } from 'styled-components'
-
-import { themeLight } from './config/theme'
-
 
 import './App.css'
 
 function App() {
   return (
     <Provider store={store} >
-      <ThemeProvider theme={themeLight}>
-      	<Routes />
-      </ThemeProvider>
+      <Routes />
     </Provider>
   )
 }
