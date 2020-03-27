f81ed348786ef52d788d65b36553b657176c2ee5
Sakhiri Mostafa <mostafa.sakhiri@gmail.com>
2020-03-27 13:42:46 +0100

    Add logout button in game page

--- src/screens/game.js
+++ src/screens/game.js
@@ -16,6 +16,9 @@ const Game = (props) => {
         score={props.boardState.score}
         refreshBoard={props.actions.board.refreshBoard}
       />
+      <LogOutButtonContainer>
+        <LogOutButton onClick={props.actions.user.logout}>LOG OUT</LogOutButton>
+      </LogOutButtonContainer>
     </GameContainer>
   );
 };
@@ -30,6 +33,22 @@ const GameContainer = styled.div`
   text-align: center;
 `
 
+const LogOutButtonContainer = styled.div`
+  position: absolute;
+  bottom: 50px;
+  width: 100%;
+  text-align: center;
+`
+
+const LogOutButton = styled.button`
+  padding: 10px 30px;
+  border-radius: 4px;
+  border: 1px solid lightgray;
+  background: lightgray;
+  color: #222;
+  font-weight: bold;
+`
+
 const mapStateToProps = state => ({
   boardState: state.board,
   themeState: state.theme
@@ -38,7 +57,8 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = () => dispatch => ({
   actions: {
     board: bindActionCreators(allTheActions.board, dispatch),
-    theme: bindActionCreators(allTheActions.theme, dispatch)
+    theme: bindActionCreators(allTheActions.theme, dispatch),
+    user: bindActionCreators(allTheActions.user, dispatch)
   }
 })
 
