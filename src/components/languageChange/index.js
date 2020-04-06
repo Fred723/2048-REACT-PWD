354b1e6ebc56f9968a785323bdfb3b1e11750896
Fred723 <frederic.nadaradjane@ynov.com>
2020-04-06 16:49:33 +0200

    Styled Languages buttons

--- src/components/languageChange/index.js
+++ src/components/languageChange/index.js
@@ -3,22 +3,40 @@ import styled from 'styled-components'
 import i18next from 'i18next'
 import { withTranslation } from 'react-i18next'
 
-const languageChange = () => {
+const languageChange = ({t}) => {
     return (
-        <div>
+        <languageChangeContainer>
+            {t('change_lang')}
+        <ButtonsContainer>
             <StyledChange onClick={() => i18next.changeLanguage('fr')}>
             fr
             </StyledChange>
             <StyledChange onClick={() => i18next.changeLanguage('en')}>
             en
             </StyledChange>
+        </ButtonsContainer>
 
-        </div>
+        </languageChangeContainer>
+
+       
     );
 };
 
 const StyledChange = styled.button`
+padding: 10px 30px;
+border-radius: 4px;
+border: 1px solid lightgray;
+background: lightgray;
+color: #222;
+font-weight: bold;
+text-transform: uppercase;
 
 `
+
+const ButtonsContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`
+
 
 export default withTranslation()(languageChange);
